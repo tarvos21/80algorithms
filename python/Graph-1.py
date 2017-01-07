e102f0a0e2a0d68dea7f27f06df50588734484e6
DuBing <tarvos21@live.com>
2017-01-07 17:07:39 +0800

    update code

--- python/Graph-1.py
+++ python/Graph-1.py
@@ -4,35 +4,35 @@ from collections import defaultdict
 class Graph:
 
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.graph = defaultdict(list)    # A
 
     def addEdge(self, boy, girl):
-        self.graph[boy].append(girl)
+        self.graph[boy].append(girl)    # B
 
     def BFS(self, start):
-        visited = [False] * (len(self.graph))
+        visited = [False] * (len(self.graph))    # C
         queue = []
         queue.append(start)
-        visited[start] = True
+        visited[start] = True    # D
 
         while queue:
             start = queue.pop(0)
-            print(start, end = ' | ')
+            print(start, end=' | ')    # E
 
             for i in self.graph[start]:
                 if visited[i] is False:
                     queue.append(i)
-                    visited[i] = True
+                    visited[i] = True    # F
 
 
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
+graph = Graph()
+graph.addEdge(0, 1)    # G
+graph.addEdge(0, 2)
+graph.addEdge(1, 2)
+graph.addEdge(2, 0)
+graph.addEdge(2, 3)
+graph.addEdge(3, 3)
 
 
 print("广度优先搜索的结果是(从2开始)：")
-g.BFS(2)
+graph.BFS(2)    # H
